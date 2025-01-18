958e2480300c099e97091bafed5496feaf040a53
LookupYUNDO <ydh7804@gmail.com>
2025-01-18 14:58:26 +0900

    route&redux공부

--- 12.redux-toolkit/redux-toolkit/src/index.js
+++ 12.redux-toolkit/redux-toolkit/src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    // #3. Provider로 Store 연결
+    // - React와 Redux 연결 : 모든 컴포넌트가 Redux 스토어에 접근할 수 있게 됨.
+    <Provider store={store}>
+        <App />
+    </Provider>
+);  
