1ae4ee3c6f73646892cf1d6d95f8054e5f98ec22
LookupYUNDO <ydh7804@gmail.com>
2025-01-21 22:19:28 +0900

    typescript 공부

--- 13.typescricpt/index.ts
+++ 13.typescricpt/index.ts
@@ -1,11 +1,6 @@
 console.log('HI');
 
-const msg : string = '타입스크립트';
-
-const num :number = 100;
-
-console.log('msg>>>' , msg);
-console.log('num>>>>' , num);
-
-
-
+const msg: string = '타입 스크립트 사용';
+const num: number = 100;
+console.log('msg >>>> ', msg);
+console.log('num >>>> ', num);
